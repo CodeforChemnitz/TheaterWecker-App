9e2865b7998f571cc382c30aa4cd9bb5e21a341c
Ronny Hartenstein <ronny@rh-flow.de>
2017-11-23 20:37:09 +0100

    fix import

--- src/components/form/Selection.js
+++ src/components/form/Selection.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import styles from '../../styles'
 
 export default class Selection extends Component {
   render() {
